aa3ea35e3854c97767d9eb32bcff0572661ae7c8
Gabriel Falcão <gabriel@nacaolivre.org>
2022-01-08 23:56:25 +0100

    TDD [red]: failing test to parse line

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+pub struct Event {
+    name: String,
+    time: String,
+    bsd_name: Option<String>,
+    volume_path: Option<String>,
+    volume_kind: Option<String>,
+    volume_name: Option<String>,
+}
+
+impl Event {
+    pub fn empty() -> Event {
+        Event {
+            name: String::new(),
+            time: String::new(),
+            bsd_name: None,
+            volume_path: None,
+            volume_kind: None,
+            volume_name: None,
+        }
+    }
+    pub fn from_line(line: &str) -> Event {
+        let mut event = Event::empty();
+        event
+    }
+    pub fn set_name(&mut self, name: &str) {
+        self.name = String::from(name);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Event;
+    use k9::assert_equal;
+
+    #[test]
+    fn test_parse_disk_appeared_without_volume_path() {
+        let line = String::from("DiskAppeared ('disk3s1', DAVolumePath = '<null>', DAVolumeKind = 'msdos', DAVolumeName = 'EFI') Time=20220108-20:22:05.1454");
+        let disk_appeared = Event::from_line(line.as_str());
+
+        assert_equal!(disk_appeared.name.as_str(), "DiskAppeared");
+    }
+}
+
+fn main() {}
