f8d31a4c06c31419af804aa23ef431c857c651c9
Gabriel Falcão <gabriel@nacaolivre.org>
2022-01-09 00:20:34 +0100

    TDD [red]: failing assertion to parse bsd name of volume

--- src/main.rs
+++ src/main.rs
@@ -25,9 +25,11 @@ impl Event {
     pub fn from_line(line: &str) -> Event {
         let mut event = Event::empty();
         //\s*\(('(?P<bsd_name>[^']+)')?, DAVolumePath\s*=\s*(?P<path>'[^']+')\)
-        let re = Regex::new(r"^[*]{3}(?P<ename>\w+)").unwrap();
+        let re = Regex::new(r"^[*]{3}(\w+)\s*\(('([^']+)')?,\s*.*\)").unwrap();
         for cap in re.captures_iter(line) {
             event.set_name(&cap[1]);
+            //event.set_bsd_name(&cap[3]);
+            //event.set_path(&cap[4]);
         }
 
         event
@@ -42,8 +44,17 @@ impl Event {
     pub fn set_name(&mut self, name: &str) {
         self.name = Some(String::from(name));
     }
+    pub fn set_bsd_name(&mut self, bsd_name: &str) {
+        self.bsd_name = Some(String::from(bsd_name));
+    }
+    pub fn bsd_name(&self) -> Option<String> {
+        self.bsd_name.clone()
+    }
+    pub fn set_path(&mut self, path: &str) {
+        self.volume_path = Some(String::from(path));
+    }
     pub fn path(&self) -> Option<String> {
-        Some(String::from("/Volumes/Time%20Machine%20Backups/"))
+        self.volume_path.clone()
     }
 }
 
@@ -58,6 +69,7 @@ mod tests {
         let disk_appeared = Event::from_line(line.as_str());
 
         assert_equal!(disk_appeared.name().as_str(), "DiskAppeared");
+        assert_equal!(disk_appeared.bsd_name(), Some(String::from("disk4")));
         assert_equal!(
             disk_appeared.path(),
             Some(String::from("/Volumes/Time%20Machine%20Backups/"))
