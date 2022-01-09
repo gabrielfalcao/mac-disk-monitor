ff0d504fd815957ef1a2d1e0905e33d8ce59069d
Gabriel Falcão <gabriel@nacaolivre.org>
2022-01-09 05:40:00 +0100

    fix: examples

--- examples/basic.rs
+++ examples/basic.rs
@@ -4,7 +4,7 @@ use std::time::Duration;
 
 fn main() {
     let (action, receiver) = channel();
-    let (thread, receiver) = stream_events("/usr/sbin/diskutil", vec!["activity"], receiver);
+    let (thread, receiver) = stream_events(receiver);
 
     loop {
         match receiver.recv_timeout(Duration::from_secs(1)) {
